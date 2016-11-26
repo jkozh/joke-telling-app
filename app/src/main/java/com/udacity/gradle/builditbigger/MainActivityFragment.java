85bf1fc2518487406b7cfd4cee22fdeb00c12b65
Julia Kozhukhovskaya <julia.kozh@yandex.ru>
2016-11-25 18:07:53 -0800

    feat: Change ActionBarActivity to AppCompatActivity in MainActivity

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -10,9 +10,6 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {

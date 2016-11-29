8a7b872bf27f16ba7f1b0ad0397b3aa074fb0173
julia <jkozhukhovskaya@gmail.com>
2016-11-29 03:24:45 -0800

    feat: Create a Java library that provides jokes

---
+++ jokes-lib/src/main/java/com/example/JokesFactory.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class JokesFactory {
+
+    public static String getRandomJoke() {
+        return "In Samsung Galaxy S7, the video will pause if you look away.\n" +
+                "In S8 hand will come out and slap you if you look away while watching a video.";
+    }
+
+}

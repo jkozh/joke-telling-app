95a6919ec822d7026aab9d4247cadfb26d3b10e6
julia <jkozhukhovskaya@gmail.com>
2016-11-29 03:34:10 -0800

    feat: Make the button display a toast showing a joke retrieved from the joke telling library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokesFactory;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, JokesFactory.getRandomJoke(), Toast.LENGTH_SHORT).show();
     }
 
 

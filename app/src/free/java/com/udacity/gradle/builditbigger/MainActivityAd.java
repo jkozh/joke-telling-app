2e282ff39752810baccdc54161b4ffa726b0aba0
julia <jkozhukhovskaya@gmail.com>
2016-11-29 23:16:17 -0800

    feat: Add Interstitial Ad to the free version

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityAd.java
@@ -0,0 +1,90 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.julia.android.jokesviewer.JokesViewerActivity;
+
+import static com.julia.android.jokesviewer.JokesViewerActivity.EXTRA_JOKE;
+
+
+public class MainActivityAd extends AppCompatActivity {
+
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                tellJoke(null);
+            }
+        });
+
+        requestNewInterstitial();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+
+        new FetchJokeTask(new FetchJokeTask.Listener() {
+
+            @Override
+            public void onJokeLoaded(String joke) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    // Pass the joke from the Java Library to the Android Library
+                    Intent sendIntent = new Intent(getApplicationContext(), JokesViewerActivity.class);
+                    sendIntent.putExtra(EXTRA_JOKE, joke);
+                    startActivity(sendIntent);
+                }
+            }
+        }).execute();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("SEE_YOUR_LOGCAT_TO_GET_YOUR_DEVICE_ID")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+}
